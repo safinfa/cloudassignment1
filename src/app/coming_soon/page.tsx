7e63a86bbab0995377262e8190d0450bbdada8ef
safinfa <Safinfarrashidiq@gmail.com>
2025-10-08 21:44:57 +0700

    coming soon page

--- src/app/coming_soon/page.tsx
+++ src/app/coming_soon/page.tsx
@@ -1,5 +1,3 @@
-
-// app/comingsoon/page.tsx
 import Link from "next/link";
 
 export default function ComingSoon() {
@@ -7,7 +5,7 @@ export default function ComingSoon() {
     <main className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
       <div className="max-w-xl w-full mx-4 p-8 border rounded shadow-sm text-center bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
         <h1 className="text-4xl font-bold mb-3 text-gray-900 dark:text-white">
-          🚧 Coming Soon 🚧
+            Coming Soon
         </h1>
         <p className="mb-6 text-gray-700 dark:text-gray-300">
           This page is currently under construction. Please check back later!
@@ -16,7 +14,7 @@ export default function ComingSoon() {
           href="/"
           className="px-4 py-2 border rounded text-gray-800 dark:text-gray-200 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 transition"
         >
-          ← Back to Home
+          Back to Home
         </Link>
       </div>
     </main>
